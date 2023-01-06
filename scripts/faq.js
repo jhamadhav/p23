5ed999e3813a222662499b8a679852b759a409b2
jhamadhav <jhamadhav28@gmail.com>
2023-01-06 15:19:43 +0530

    qr added

--- scripts/faq.js
+++ scripts/faq.js
@@ -2,10 +2,7 @@ let qs = document.getElementsByClassName("faq-qs")
 for (let i = 0; i < qs.length; ++i) {
     qs[i].onclick = () => {
         let ans = document.getElementsByClassName("faq-ans")
-        if (ans[i].style.display == "none") {
-            ans[i].style.display = "block"
-        } else {
-            ans[i].style.display = "none"
-        }
+        let temp = (ans[i].style.display != "block")
+        ans[i].style.display = (temp) ? "block" : "none"
     }
 }
