5e4141ffe5beb1d88fb86acbaf46d1695b9c6052
Supraj <suprajgijre@gmail.com>
2023-01-22 23:52:15 +0530

    updated carousal

--- scripts/event-details.js
+++ scripts/event-details.js
@@ -3,121 +3,142 @@ let events = [
         "name": "INGENIUM",
         "date": "2nd Jan, 2023",
         "about": "Hello amigos! We feel pleasure bringing the most awaited and playful event of Pratishruti' 23-INGENIUM .This event will have multiple stages and we guarantee that you will have loads of fun.We wait to see you pull the strings and hunt to win the game.",
-        "image": "ingenium.png"
+        "image": "ingenium.png",
+        "link" : "https://rcoem.in/anon_evForm.htm?ev=1900"
+
     },
     {
         "name": "PERSONA",
         "date": "2nd Jan, 2023",
         "about": "Buenos dias.Beauty gets the attention ,personality gets the heart .Everyone thinks they are the King or the Queen of their own world , so we are glad to host an event where the Alexander and Elizabeth will be decided.",
-        "image": "persona.png"
+        "image": "persona.png",
+        "link" : "https://rcoem.in/anon_evForm.htm?ev=1900"
     },
     {
         "name": "DALAL STREET",
         "date": "2nd Jan, 2023",
         "about": "'Wake up ! Its 9:15 am already'-We are trying to create a vibrant hub of commerce and finance.Trying to replicate a place where fortunes of millions are made right here in the campus.Whether you're a seasoned player or a newcomer to the game, Dalal Street is a place where anyone can make their mark. ",
-        "image": "DalalStreet.png"
+        "image": "DalalStreet.png",
+        "link" : "https://rcoem.in/anon_evForm.htm?ev=1900"
     },
     {
         "name": "MINUTE TO WIN IT",
         "date": "2nd Jan, 2023",
         "about": "Minute to Win It is an event that involves contestants completing simple, yet challenging tasks and games using the given items.This event is known for its high-energy atmosphere and its wacky and fun challenges.We wait to welcome you in the mania of maniacs.",
-        "image": "MinuteToWInIt.png"
+        "image": "MinuteToWInIt.png",
+        "link" : "https://rcoem.in/anon_evForm.htm?ev=1900"
     },
     {
         "name": "WILD VOYAGE",
         "date": "2nd Jan, 2023",
         "about": "'wild Voyage' as the name suggests is our luck for this wild journey of fun events .So don't be afraid to take that next big step - with a hearty 'bon voyage' in your pocket, you'll be ready to take on the world and make your mark on the map.",
-        "image": "Bon-Voyage-2.png"
+        "image": "Bon-Voyage-2.png",
+        "link" : "https://rcoem.in/anon_evForm.htm?ev=1900"
     },
     {
         "name": "NATURE'S VOICE",
         "date": "2nd Jan, 2023",
         "about": "  Become the one who breaks all the rules and turns the volume down to zero! Grove your moves to 'The Breakup song' !",
-        "image": "NaturesVoices.png"
+        "image": "NaturesVoices.png",
+        "link" : "https://rcoem.in/anon_evForm.htm?ev=1900"
     },
     {
         "name": "E-SPORTS",
         "date": "2nd Jan, 2023",
         "about": " Welcome to the world of eSports, where professional video game players face off in epic battles of strategy and skill. We appreciate the digital athletes who train tirelessly, perfecting their craft and mastering their favorite games. So grab your controller, pick your champions, and get ready to join the fray .",
-        "image": "ESports2.png"
+        "image": "ESports2.png",
+        "link" : "https://rcoem.in/anon_evForm.htm?ev=1900"
     },
     {
         "name": "THE MEME-ORIAL",
         "date": "2nd Jan, 2023",
         "about": " 'Welcome to the wild and wacky world of the 'Meme-orandum'! This is your chance to flex your comedic muscles and show off your meme-making skills. Whether you're a meme aficionado or a novice, everyone is welcome to join in on the fun.",
-        "image": "MEME_Temp.jpg"
+        "image": "MEME_Temp.jpg",
+        "link" : "https://rcoem.in/anon_evForm.htm?ev=1900"
     },
     {
         "name": "PURANE DIN",
         "date": "2nd Jan, 2023",
         "about": " Become the one who breaks all the rules and turns the volume down to zero! Grove your moves to 'The Breakup song' !",
-        "image": "Purane_din.png"
+        "image": "Purane_din.png",
+        "link" : "https://rcoem.in/anon_evForm.htm?ev=1900"
     },
     
     {
         "name": "SQUID GAMES",
         "date": "2nd Jan, 2023",
         "about": " You may feel like it is long and boring series, you may be right about it but this event is gonna be exact opposite of it, infact it is gonna be more interesting than 'Money Heist' and 'ups and downs'.We await to welcome you to the madness of 'squid games'",
-        "image": "Squid game-01.png"
+        "image": "Squid game-01.png",
+        "link" : "https://rcoem.in/anon_evForm.htm?ev=1900"
     },
     {
         "name": "DEDH SAU RUPYA DEGA",
         "date": "2nd Jan, 2023",
         "about": "Who has not heard the famous dialogue of Kachra Seth '150 rupya dega' but instead of taking we gonna give and make it a hell of fun for all the kiddos saying 'I am gonna be a millionaire' .",
-        "image": "dedh sau rupya dega-1(updated(2)).png"
+        "image": "dedh sau rupya dega-1(updated(2)).png",
+        "link" : "https://rcoem.in/anon_evForm.htm?ev=1900"
     },
     {
         "name": "KHANA-KHAZANA",
         "date": "2nd Jan, 2023",
         "about": " Step right up and test your limits at our exciting skill-based and speedy event which is not only gonna be a simple race but a mix of spices to test your taste ",
-        "image": "Khana-khazana.png"
+        "image": "Khana-khazana.png",
+        "link" : "https://rcoem.in/anon_evForm.htm?ev=1900"
     },
     {
         "name": "QUIZZEUS",
         "date": "2nd Jan, 2023",
         "about": "If your common sense is not so common,if your IQ is more than your competitor ,if you can feel the importance of a single second and manage your time to the best of your abilities, then it is the area where you can ace and get a hand above others.",
-        "image": "QUIZZEUS_new.png"
+        "image": "QUIZZEUS_new.png",
+        "link" : "https://rcoem.in/anon_evForm.htm?ev=1900"
     },
     {
         "name": "BEATMAKER'S ARENA",
         "date": "2nd Jan, 2023",
         "about": " If your beats can make others swing and mixing recorded music is your passion, survive this great DJ war and open up EDM Night!",
-        "image": "beatmaker_sArena.png"
+        "image": "beatmaker_sArena.png",
+        "link" : "https://rcoem.in/anon_evForm.htm?ev=1900"
     },
     {
         "name": "FOOTSTEPS",
         "date": "2nd Jan, 2023",
         "about": " Become the one who breaks all the rules and turns the volume down to zero! Grove your moves to 'The Breakup song' !",
-        "image": "FootSteps_0.png"
+        "image": "FootSteps_0.png",
+        "link" : "https://rcoem.in/anon_evForm.htm?ev=1900"
     },
     {
         "name": "VIBRATO",
         "date": "2nd Jan, 2023",
         "about": "This one's for your love of music! Prove your own melodic mettle by taking part in Vibrato. Arenas include solo or duets for vocals and solo or group for instrumentals.",
-        "image": "vibrato.png"
+        "image": "vibrato.png",
+        "link" : "https://rcoem.in/anon_evForm.htm?ev=1900"
     },
     {
         "name": "NAQAAB",
         "date": "2nd Jan, 2023",
         "about": "“Drama lies in extreme exaggeration of the feelings, an exaggeration that dislocates flat everyday reality.” NAQAAB will unveil a commitment to the art of theater, to the cultural fabric of the audience, bringing to them a wholesome feast of drama to awaken and thrill the senses. So gather the troupe, watch and perform ingeniously written scripts, transforming them into mesmerizing plays, sparred up with the dexterity and forte of actors guided by adept direction.",
-        "image": "naqaab.png"
+        "image": "naqaab.png",
+        "link" : "https://rcoem.in/anon_evForm.htm?ev=1900"
     },
     {
         "name": "ART AFFAIR",
         "date": "2nd Jan, 2023",
         "about": "Face Painting, Wall Painting, Fashion Desiging- Create cool dresses from crafts",
-        "image": "artaffairs.png"
+        "image": "artaffairs.png",
+        "link" : "https://rcoem.in/anon_evForm.htm?ev=1900"
     },
     {
         "name": "PICASSO",
         "date": "2nd Jan, 2023",
         "about": " Become the one who breaks all the rules and turns the volume down to zero! Grove your moves to 'The Breakup song' !",
-        "image": "picasso.png"
+        "image": "picasso.png",
+        "link" : "https://rcoem.in/anon_evForm.htm?ev=1900"
     },
     {
         "name": "RCOEM LITERATURE FESTIVAL",
         "date": "2nd Jan, 2023",
         "about": "Where words are brewed to produce fine elixir of sentiments, immersing you  into a world of poems, stories and chronicles",
-        "image": "event1.png"
+        "image": "event1.png",
+        "link" : "https://rcoem.in/anon_evForm.htm?ev=1900"
     },
 ]
