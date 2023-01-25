ab1658844b38a1315caaa7dbb1258eb8036ae761
jhamadhav <jhamadhav28@gmail.com>
2023-01-25 19:57:48 +0530

    show only next five events on event carousal

--- scripts/event-details.js
+++ scripts/event-details.js
@@ -2,6 +2,7 @@ let events = [
     {
         "name": "INGENIUM",
         "date": "26th Jan, 2023",
+        "time": "2023-01-26",
         "about": "Hello amigos! We feel pleasure bringing the most awaited and playful event of Pratishruti' 23-INGENIUM .This event will have multiple stages and we guarantee that you will have loads of fun.We wait to see you pull the strings and hunt to win the game.",
         "image": "ingenium.png",
         "link": "https://rcoem.in/anon_evForm.htm?ev=1900"
@@ -10,6 +11,7 @@ let events = [
     {
         "name": "PERSONA",
         "date": "26th Jan, 2023",
+        "time": "2023-01-26",
         "about": "Buenos dias.Beauty gets the attention ,personality gets the heart .Everyone thinks they are the King or the Queen of their own world , so we are glad to host an event where the Alexander and Elizabeth will be decided.",
         "image": "persona.png",
         "link": "https://rcoem.in/anon_evForm.htm?ev=1900"
@@ -17,6 +19,7 @@ let events = [
     {
         "name": "DALAL STREET",
         "date": "27th Jan, 2023",
+        "time": "2023-01-27",
         "about": "'Wake up ! Its 9:15 am already'-We are trying to create a vibrant hub of commerce and finance.Trying to replicate a place where fortunes of millions are made right here in the campus.Whether you're a seasoned player or a newcomer to the game, Dalal Street is a place where anyone can make their mark. ",
         "image": "DalalStreet.png",
         "link": "https://rcoem.in/anon_evForm.htm?ev=1900"
@@ -24,6 +27,7 @@ let events = [
     {
         "name": "MINUTE TO WIN IT",
         "date": "28th Jan, 2023",
+        "time": "2023-01-28",
         "about": "Minute to Win It is an event that involves contestants completing simple, yet challenging tasks and games using the given items.This event is known for its high-energy atmosphere and its wacky and fun challenges.We wait to welcome you in the mania of maniacs.",
         "image": "MinuteToWInIt.png",
         "link": "https://rcoem.in/anon_evForm.htm?ev=1900"
@@ -32,6 +36,7 @@ let events = [
     {
         "name": "NATURE'S VOICE",
         "date": "29th Jan, 2023",
+        "time": "2023-01-29",
         "about": "  Become the one who breaks all the rules and turns the volume down to zero! Grove your moves to 'The Breakup song' !",
         "image": "NaturesVoices.png",
         "link": "https://rcoem.in/anon_evForm.htm?ev=1900"
@@ -39,6 +44,7 @@ let events = [
     {
         "name": "E-SPORTS",
         "date": "1st Feb, 2023",
+        "time": "2023-02-01",
         "about": " Welcome to the world of eSports, where professional video game players face off in epic battles of strategy and skill. We appreciate the digital athletes who train tirelessly, perfecting their craft and mastering their favorite games. So grab your controller, pick your champions, and get ready to join the fray .",
         "image": "ESports2.png",
         "link": "https://rcoem.in/anon_evForm.htm?ev=1900"
@@ -46,6 +52,7 @@ let events = [
     {
         "name": "THE MEME-ORIAL",
         "date": "25th Jan, 2023",
+        "time": "2023-01-25",
         "about": " 'Welcome to the wild and wacky world of the 'Meme-orandum'! This is your chance to flex your comedic muscles and show off your meme-making skills. Whether you're a meme aficionado or a novice, everyone is welcome to join in on the fun.",
         "image": "MEME_Temp.jpg",
         "link": "https://rcoem.in/anon_evForm.htm?ev=1900"
@@ -54,6 +61,7 @@ let events = [
     {
         "name": "SQUID GAMES",
         "date": "25th Jan, 2023",
+        "time": "2023-01-25",
         "about": " You may feel like it is long and boring series, you may be right about it but this event is gonna be exact opposite of it, infact it is gonna be more interesting than 'Money Heist' and 'ups and downs'.We await to welcome you to the madness of 'squid games'",
         "image": "Squid game-01.png",
         "link": "https://rcoem.in/anon_evForm.htm?ev=1900"
@@ -61,6 +69,7 @@ let events = [
     {
         "name": "DEDH SAU RUPYA DEGA",
         "date": "27th Jan, 2023",
+        "time": "2023-01-27",
         "about": "Who has not heard the famous dialogue of Kachra Seth '150 rupya dega' but instead of taking we gonna give and make it a hell of fun for all the kiddos saying 'I am gonna be a millionaire' .",
         "image": "dedh sau rupya dega-1(updated(2)).png",
         "link": "https://rcoem.in/anon_evForm.htm?ev=1900"
@@ -68,6 +77,7 @@ let events = [
     {
         "name": "KHANA-KHAZANA",
         "date": "27th Jan, 2023",
+        "time": "2023-01-27",
         "about": " Step right up and test your limits at our exciting skill-based and speedy event which is not only gonna be a simple race but a mix of spices to test your taste ",
         "image": "Khana-khazana.png",
         "link": "https://rcoem.in/anon_evForm.htm?ev=1900"
@@ -75,6 +85,7 @@ let events = [
     {
         "name": "QUIZZEUS",
         "date": "28th Jan, 2023",
+        "time": "2023-01-28",
         "about": "If your common sense is not so common,if your IQ is more than your competitor ,if you can feel the importance of a single second and manage your time to the best of your abilities, then it is the area where you can ace and get a hand above others.",
         "image": "QUIZZEUS_new.png",
         "link": "https://rcoem.in/anon_evForm.htm?ev=1900"
@@ -82,6 +93,7 @@ let events = [
     {
         "name": "BEATMAKER'S ARENA",
         "date": "30th Jan, 2023",
+        "time": "2023-01-30",
         "about": " If your beats can make others swing and mixing recorded music is your passion, survive this great DJ war and open up EDM Night!",
         "image": "beatmaker_sArena.png",
         "link": "https://rcoem.in/anon_evForm.htm?ev=1900"
@@ -89,6 +101,7 @@ let events = [
     {
         "name": "FOOTSTEPS",
         "date": "27th Jan, 2023",
+        "time": "2023-01-27",
         "about": " Become the one who breaks all the rules and turns the volume down to zero! Grove your moves to 'The Breakup song' !",
         "image": "FootSteps_0.png",
         "link": "https://rcoem.in/anon_evForm.htm?ev=1900"
@@ -96,6 +109,7 @@ let events = [
     {
         "name": "VIBRATO",
         "date": "28th Jan, 2023",
+        "time": "2023-01-28",
         "about": "This one's for your love of music! Prove your own melodic mettle by taking part in Vibrato. Arenas include solo or duets for vocals and solo or group for instrumentals.",
         "image": "vibrato.png",
         "link": "https://rcoem.in/anon_evForm.htm?ev=1900"
@@ -103,6 +117,7 @@ let events = [
     {
         "name": "NAQAAB",
         "date": "31st Jan, 2023",
+        "time": "2023-01-31",
         "about": "“Drama lies in extreme exaggeration of the feelings, an exaggeration that dislocates flat everyday reality.” NAQAAB will unveil a commitment to the art of theater, to the cultural fabric of the audience, bringing to them a wholesome feast of drama to awaken and thrill the senses. So gather the troupe, watch and perform ingeniously written scripts, transforming them into mesmerizing plays, sparred up with the dexterity and forte of actors guided by adept direction.",
         "image": "naqaab.png",
         "link": "https://rcoem.in/anon_evForm.htm?ev=1900"
@@ -110,6 +125,7 @@ let events = [
     {
         "name": "ART AFFAIR",
         "date": "27th Jan, 2023",
+        "time": "2023-01-27",
         "about": "Face Painting, Wall Painting, Fashion Desiging- Create cool dresses from crafts",
         "image": "artaffairs.png",
         "link": "https://rcoem.in/anon_evForm.htm?ev=1900"
@@ -117,6 +133,7 @@ let events = [
     {
         "name": "PICASSO",
         "date": "26th Jan, 2023",
+        "time": "2023-01-26",
         "about": " Become the one who breaks all the rules and turns the volume down to zero! Grove your moves to 'The Breakup song' !",
         "image": "picasso.png",
         "link": "https://rcoem.in/anon_evForm.htm?ev=1900"
@@ -124,6 +141,7 @@ let events = [
     {
         "name": "RCOEM LITERATURE FESTIVAL",
         "date": "30th Jan, 2023",
+        "time": "2023-01-30",
         "about": "Where words are brewed to produce fine elixir of sentiments, immersing you  into a world of poems, stories and chronicles",
         "image": "event1.png",
         "link": "https://rcoem.in/anon_evForm.htm?ev=1900"
